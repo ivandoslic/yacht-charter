0a99f2beca85cc7fe03d90189ab74b3c4f7beaba
Ivan Doslic <doslicivan03@gmail.com>
2024-02-26 23:45:10 +0100

    fixed typo

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Dropdown from './DropDown'
+import Dropdown from './Dropdown'
 import TextInput from './TextInput'
 import { UserIcon, InboxIcon } from '@heroicons/react/24/outline'
 
